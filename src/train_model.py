773ec7d7f070eeec0687e4c5c8adf49abbd72560
s184191 <s184191@student.dtu.dk>
2024-01-11 11:40:36 +0100

    Add first data and model logic

--- src/train_model.py
+++ src/train_model.py
@@ -1,32 +1,12 @@
-import os
-import os.path as path
+from datasets import load_from_disk
+from happytransformer import HappyTextToText, TTTrainArgs
 
-import matplotlib.pyplot as plt
-import torch
-from pytorch_lightning import Trainer
+def train(train_data_path):
+    model = HappyTextToText('t5-small')
+    args = TTTrainArgs(batch_size=8)
 
-from models.model import MyAwesomeModel
+    model.train(train_data_path, args=args)
 
-
-def main():
-    print("Training day and night")
-
-    model = MyAwesomeModel()
-
-    epochs = 20
-    trainer = Trainer(max_epochs=epochs)
-    trainer.fit(model)
-
-    plt.plot(list(range(1, epochs + 1)), model.train_losses)
-    plt.title("Training results")
-    plt.xlabel("Epoch")
-    plt.ylabel("Loss")
-    plt.savefig(path.join(os.getcwd(), "reports/figures/training_results.png"), dpi=200)
-
-    trainer.test(model)
-
-    torch.save(model.state_dict(), path.join(os.getcwd(), "models/trained_model.pt"))
-
-
-if __name__ == "__main__":
-    main()
+if __name__=='__main__':
+    PATH='data/processed/train.csv'
+    train(PATH)
