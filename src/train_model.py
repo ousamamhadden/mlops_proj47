44361982668e0d48ced489eb9a2b2290c28368ac
ousamamhadden <ousamamhadden@outlook.com>
2024-01-14 09:57:25 +0100

    wandb

--- src/train_model.py
+++ src/train_model.py
@@ -8,8 +8,7 @@ os.environ["WANDB_LOG_MODEL"] = "checkpoint"
 @hydra.main(config_path='models/config', config_name="config.yaml")
 def train(cfg):
     model = HappyTextToText('t5-small')
-    args = TTTrainArgs(batch_size=cfg.batch_size, report_to='wandb')
-
+    args = TTTrainArgs(batch_size=cfg.batch_size, report_to=tuple(["wandb"]),project_name="mlops-47",run_name="test")
     model.train(cfg.dataset_path, args=args)
     beam_settings =  TTSettings(num_beams=5, min_length=1, max_length=100)
     input_text_1 = "grammar: I I wants to codes."
