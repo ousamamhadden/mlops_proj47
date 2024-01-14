5193fb02bb9f2a51f8b4a81c9bc81f61ca1d7db5
ousamamhadden <ousamamhadden@outlook.com>
2024-01-14 09:14:54 +0100

    test123

--- src/train_model.py
+++ src/train_model.py
@@ -1,6 +1,6 @@
 import os
 import hydra
-from happytransformer import HappyTextToText, TTTrainArgs
+from happytransformer import HappyTextToText, TTTrainArgs,TTSettings
 
 os.environ["WANDB_PROJECT"] = "mlops-proj47"
 os.environ["WANDB_LOG_MODEL"] = "checkpoint"
@@ -11,6 +11,14 @@ def train(cfg):
     args = TTTrainArgs(batch_size=cfg.batch_size, report_to='wandb')
 
     model.train(cfg.dataset_path, args=args)
+    beam_settings =  TTSettings(num_beams=5, min_length=1, max_length=100)
+    input_text_1 = "grammar: I I wants to codes."
+    output_text_1 = model.generate_text(input_text_1, args=beam_settings)
+    print(output_text_1.text)
+    model.train(cfg.dataset_path, args=args)
+    output_text_1 = model.generate_text(input_text_1, args=beam_settings)
+    print(output_text_1.text)
+    model.save("model/")
 
 if __name__=='__main__':
     train()
