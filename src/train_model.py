a5e4b968529a64dd7b89d99793e2354c25527289
ousamamhadden <ousamamhadden@outlook.com>
2024-01-14 10:24:21 +0100

    dont use hydra buggy

--- src/train_model.py
+++ src/train_model.py
@@ -5,16 +5,15 @@ from happytransformer import HappyTextToText, TTTrainArgs,TTSettings
 os.environ["WANDB_PROJECT"] = "mlops-proj47"
 os.environ["WANDB_LOG_MODEL"] = "checkpoint"
 
-@hydra.main(config_path='models/config', config_name="config.yaml")
-def train(cfg):
+#@hydra.main(config_path='models/config', config_name="config.yaml")
+def train():
     model = HappyTextToText('t5-small')
-    args = TTTrainArgs(batch_size=cfg.batch_size, report_to=tuple(["wandb"]),project_name="mlops-47",run_name="test")
-    model.train(cfg.dataset_path, args=args)
+    args = TTTrainArgs(batch_size=8, report_to="wandb",project_name="mlops-47",run_name="test")
     beam_settings =  TTSettings(num_beams=5, min_length=1, max_length=100)
     input_text_1 = "grammar: I I wants to codes."
     output_text_1 = model.generate_text(input_text_1, args=beam_settings)
     print(output_text_1.text)
-    model.train(cfg.dataset_path, args=args)
+    model.train('data/processed/train.csv', args=args)
     output_text_1 = model.generate_text(input_text_1, args=beam_settings)
     print(output_text_1.text)
     model.save("model/")
